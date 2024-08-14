6e06e23ee58df8fd649208b66b9dc07bccffebb2
Ryan <ryguyrobertson@gmail.com>
2024-08-14 11:13:53 -0700

    add collision to player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,17 @@
-extends Node2D
+extends CharacterBody2D
 
 func _process(delta):
 	
-	# input
+	# input for movement
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
+	
+	#laser shooting input
+	if Input.is_action_pressed("primary ability"):
+		print("Shoot laser")
+		
+	
+	# grenade shooting input
+	if Input.is_action_pressed("secondary ability"):
+		print("shoot grenade")
