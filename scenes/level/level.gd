c7ac76e1f106399c94541f0cc08f174d17cef846
Ryan <ryguyrobertson@gmail.com>
2024-08-15 10:24:58 -0700

    add laser scene + spawning

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+# Preload Scene
+
+var laser_scene : PackedScene = preload("res://scenes/projectiles/laser.tscn")
+
 func _on_gate_player_entered_gate():
 	print("player has entered gate")
 
@@ -9,5 +13,13 @@ func _on_player_grenade():
 	print("Grenade from player")
 
 
-func _on_player_laser():
-	print("laser from player")
+func _on_player_laser(pos):
+	var laser = laser_scene.instantiate()
+	
+	laser.position = pos
+	# Update laser position
+	
+	# Move the laser
+	
+	# Add laser instance to a Node2D
+	add_child(laser)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var can_laser: bool = true;
 var can_grenade: bool = true;
 
-signal laser
+signal laser(position)
 signal grenade
 
 func _process(_delta):
@@ -15,20 +15,21 @@ func _process(_delta):
 	
 	#laser shooting input
 	if Input.is_action_pressed("primary ability") and can_laser:
-		print("Shoot laser")
-		laser.emit()
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		# Laser shoot delay
 		can_laser = false
 		$LaserDelay.start()
+		laser.emit(selected_laser.global_position)
+		# Emit the position
 		
 	
 	# grenade shooting input
 	if Input.is_action_pressed("secondary ability") and can_grenade:
-		print("shoot grenade")
-		grenade.emit()
 		# Grenade shoot delay
-		$GrenadeDelay.start()
 		can_grenade = false
+		$GrenadeDelay.start()
+		grenade.emit()
 
 
 func _on_laser_delay_timeout():
